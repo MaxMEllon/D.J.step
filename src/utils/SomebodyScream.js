4326c3147e585814afdabe931daf0ea81046d842
MaxMEllon <maxmellon1994@gmail.com>
2016-05-16 22:26:06 +0900

    Add: :musical_note: Sample code of reverse play

--- src/utils/SomebodyScream.js
+++ src/utils/SomebodyScream.js
@@ -1,29 +1,35 @@
+import debug from './debug';
+
 const context = new window.AudioContext();
-
-let buffer = null;
-
 const request = new XMLHttpRequest();
 
 request.open('GET', '/assets/sound/3bai.wav');
 request.responseType = 'arraybuffer';
 request.send();
 
-request.onload = () => {
-  const res = request.response;
-  context.decodeAudioData(res, buf => {
-    buffer = buf;
-  });
-};
+let res = null;
+
+request.onload = () => { res = request.response; };
 
 export default class SomebodyScream {
   constructor() {
-    this.source = context.createBufferSource();
-    this.source.buffer = buffer;
-    this.source.connect(context.destination);
+    context.decodeAudioData(res, buf => {
+      this.source = context.createBufferSource();
+      // Array.prototype.reverse.call(buf.getChannelData(0));
+      this.source.buffer = buf;
+      this.source.connect(context.destination);
+    });
   }
 
   play() {
-    this.source.start();
+    setTimeout(() => {
+      this.source.start();
+      // let rate = 1;
+      // setInterval(() => {
+      //   this.source.playbackRate.value = rate;
+      //   rate += 0.1;
+      // }, 10);
+    }, 10);
   }
 
   stop() {
